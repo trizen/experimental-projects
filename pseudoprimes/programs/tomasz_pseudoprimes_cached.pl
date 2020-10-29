e98a25ed74dacc9444e21f512c16085d297d5716
trizen <trizen@protonmail.com>
2020-10-29 03:44:25 +0200

    new file:   oeis-research/Bernard Schott/Smallest number m such that tau(k) * k = m has n solutions/smooth_generate.pl new file:   oeis-research/Naohiro Nomoto/Smallest number m such that sigma(x)*x = m has n solutions/smooth_generate.pl modified:   oeis-research/Zak Seidov/Primes p congruent to 1..8 mod 13/sieve_cluster.pl new file:   pseudoprimes/programs/tomasz_pseudoprimes_cached.pl

---
+++ pseudoprimes/programs/tomasz_pseudoprimes_cached.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+# Thomas Ordowski asked:
+#   Are there composite numbers n such that 2^n - 1 has all prime divisors p == 1 (mod n) ?
+
+# If both 2^p - 1 and 2^q - 1 are prime and n = pq is pseudoprime, then n is such a number.
+
+# However, the known Mersenne primes do not give such pseudoprimes.
+
+# http://list.seqfan.eu/pipermail/seqfan/2018-October/018943.html
+
+use 5.020;
+use strict;
+use warnings;
+
+use Storable;
+use Math::GMPz;
+use ntheory qw(:all);
+use Math::Prime::Util::GMP;
+use experimental qw(signatures);
+
+my $storable_file = "cache/factors-fermat.storable";
+my $numbers = retrieve($storable_file);
+
+while (my ($n, $value) = each %$numbers) {
+
+    my @f = split(' ', $value);
+
+    if (vecall { Math::Prime::Util::GMP::modint($_, $n) eq '1' } @f) {
+        say $n;
+    }
+}
+
+__END__
+
+# No such number is currently known.
