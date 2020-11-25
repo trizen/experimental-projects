f01ea38ebefbcf5a9977808662dec5b2e99d1f81
trizen <trizen@protonmail.com>
2020-11-25 06:42:41 +0200

    new file:   pseudoprimes/programs/lehmar_totient_weak_numbers_native.pl

--- pseudoprimes/programs/lehmar_totient_weak_numbers_cached.pl
+++ pseudoprimes/programs/lehmar_totient_weak_numbers_cached.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Composite numbers n such that phi(n) divides p*(n - 1) for some prime factor p of n - 1.
+# Composite numbers n such that phi(n) divides p*(n - 1) for some prime factor p of n-1.
 # https://oeis.org/A338998
 
 # Known terms:
@@ -23,24 +23,32 @@ my $carmichael_file = "cache/factors-carmichael.storable";
 my $carmichael = retrieve($carmichael_file);
 
 sub my_euler_phi ($factors) {   # assumes n is squarefree
-    Math::GMPz->new(Math::Prime::Util::GMP::vecprod(map{ Math::Prime::Util::GMP::subint($_, 1) } @$factors));
+    Math::Prime::Util::GMP::vecprod(map{ ($_ < ~0) ? ($_-1) : Math::Prime::Util::GMP::subint($_, 1) } @$factors);
 }
 
-my $TRIAL_LIMIT = 1e6;
+my $TRIAL_LIMIT = 1e3;
+
+my $n   = Math::GMPz::Rmpz_init();
+my $nm1 = Math::GMPz::Rmpz_init();
+my $phi = Math::GMPz::Rmpz_init();
 
 while (my ($key, $value) = each %$carmichael) {
 
-    Math::Prime::Util::GMP::gcd($key, 3*5*7) > 1 or next;
+    length($key) <= 50 or next;
+    #~ Math::Prime::Util::GMP::gcd($key, 3*5*7) > 1 or next;
 
     my @factors = split(' ', $value);
 
-    my $phi = my_euler_phi(\@factors);
-    my $n = Math::GMPz->new($key);
-    my $nm1 = $n-1;
+    $factors[0] < 100 or next;
 
+    Math::GMPz::Rmpz_set_str($phi, my_euler_phi(\@factors), 10);
+    Math::GMPz::Rmpz_set_str($n, $key, 10);
+    Math::GMPz::Rmpz_sub_ui($nm1, $n, 1);
+
+    my %seen;
     my @trial = trial_factor($nm1, $TRIAL_LIMIT);
 
-    if (vecany { ($$_ < $TRIAL_LIMIT) && ((($nm1 * $$_) % $phi) == 0) } @trial) {
+    if (vecany { !$seen{$$_}++ && ($$_ < $TRIAL_LIMIT) && ((($nm1 * $$_) % $phi) == 0) } @trial) {
         say $key;
     }
 }
