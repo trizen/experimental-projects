a92410cd4ba882572e61758a21bb9ad368b4bdaa
trizen <trizen@protonmail.com>
2020-12-14 13:03:37 +0200

    modified:   oeis-research/Robert Israel/Numbers n such that binary(n) == 1 mod n/prog.c new file:   oeis-research/Robert Israel/Numbers n such that binary(n) == 1 mod n/prog.cpp

--- oeis-research/Robert Israel/Numbers n such that binary(n) == 1 mod n/prog.c
+++ oeis-research/Robert Israel/Numbers n such that binary(n) == 1 mod n/prog.c
@@ -1,33 +1,38 @@
 
 // Compilation:
-//  g++ -march=native -Ofast -lgmp prog.c -o prog
+//  gcc -march=native -Ofast -lgmp prog.c -o prog
 
 // Numbers n such that A007088(n) == 1 (mod n).
 // https://oeis.org/A339567
 
-// New terms found:
-//  1839399055, 7786281065, 11231388063, 17251448809, 71050380625
-
 #include <gmp.h>
-#include <iostream>
-#include <bitset>
-
-using namespace std;
+#include <stdio.h>
+#include <math.h>
 
 typedef unsigned long long int u64;
 
-int main() {
+char *int2bits(u64 answer, char *result) {
+    if (answer > 1) {
+        result = int2bits(answer >> 1, result);
+    }
+    *result = '0' + (answer & 0x01);
+    return result + 1;
+}
+
+int main(int argc, char *argv[]) {
+
+    char s[64];
 
     mpz_t z;
     mpz_inits(z, NULL);
 
     for (u64 n = 71050380625; n <= 1000000000000; n += 2) {
 
-        std::string s = std::bitset< 64 >( n ).to_string();
-        mpz_set_str(z, s.c_str(), 10);
+        *int2bits(n, s) = '\0';
+        mpz_set_str(z, s, 10);
 
         if (mpz_mod_ui(z, z, n) == 1) {
-            cout << n << endl;
+            printf("%lld\n", n);
         }
     }
 }
