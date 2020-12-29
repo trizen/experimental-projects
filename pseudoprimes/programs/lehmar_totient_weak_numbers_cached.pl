542cfe734214d5d4f7ea0295d57ad39a3859a501
trizen <trizen@protonmail.com>
2020-12-29 23:43:32 +0200

    new file:   oeis-research/Tomohiro Yamada/Quadratically perfect numbers/prog.sf

--- pseudoprimes/programs/lehmar_totient_weak_numbers_cached.pl
+++ pseudoprimes/programs/lehmar_totient_weak_numbers_cached.pl
@@ -26,30 +26,51 @@ sub my_euler_phi ($factors) {    # assumes n is squarefree
     Math::Prime::Util::GMP::vecprod(map { ($_ < ~0) ? ($_ - 1) : Math::Prime::Util::GMP::subint($_, 1) } @$factors);
 }
 
-my $TRIAL_LIMIT = 1e3;
+sub is_smooth_over_prod ($n, $k) {
 
-my $n   = Math::GMPz::Rmpz_init();
+    state $g = Math::GMPz::Rmpz_init_nobless();
+    state $t = Math::GMPz::Rmpz_init_nobless();
+
+    Math::GMPz::Rmpz_set($t, $n);
+    Math::GMPz::Rmpz_gcd($g, $t, $k);
+
+    while (Math::GMPz::Rmpz_cmp_ui($g, 1) > 0) {
+        Math::GMPz::Rmpz_remove($t, $t, $g);
+        return 1 if Math::GMPz::Rmpz_cmp_ui($t, 1) == 0;
+        Math::GMPz::Rmpz_gcd($g, $t, $g);
+    }
+
+    return 0;
+}
+
+my $t   = Math::GMPz::Rmpz_init();
 my $nm1 = Math::GMPz::Rmpz_init();
 my $phi = Math::GMPz::Rmpz_init();
 
+my $primorial = Math::GMPz::Rmpz_init();
+Math::GMPz::Rmpz_primorial_ui($primorial, 1e6);
+
 while (my ($key, $value) = each %$carmichael) {
 
-    length($key) <= 50 or next;
+    length($key) <= 30 or next;
 
-    #~ Math::Prime::Util::GMP::gcd($key, 3*5*7) > 1 or next;
+    Math::GMPz::Rmpz_set_str($nm1, $key, 10);
+    Math::GMPz::Rmpz_sub_ui($nm1, $nm1, 1);
+
+    is_smooth_over_prod($nm1, $primorial) || next;
 
     my @factors = split(' ', $value);
-
-    $factors[0] < 100 or next;
-
     Math::GMPz::Rmpz_set_str($phi, my_euler_phi(\@factors), 10);
-    Math::GMPz::Rmpz_set_str($n,   $key,                    10);
-    Math::GMPz::Rmpz_sub_ui($nm1, $n, 1);
 
-    my %seen;
-    my @trial = trial_factor($nm1, $TRIAL_LIMIT);
+    my @nm1_factors = uniq(Math::Prime::Util::GMP::factor($nm1));
 
-    if (vecany { !$seen{$$_}++ && ($$_ < $TRIAL_LIMIT) && ((($nm1 * $$_) % $phi) == 0) } @trial) {
+    if (
+        vecany {
+            Math::GMPz::Rmpz_mul_ui($t, $nm1, $_);
+            Math::GMPz::Rmpz_divisible_p($t, $phi);
+        }
+        @nm1_factors
+      ) {
         say $key;
     }
 }
