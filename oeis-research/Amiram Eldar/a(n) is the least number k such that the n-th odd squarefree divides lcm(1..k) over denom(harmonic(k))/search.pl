c1c607227520449e56e119d59cb0e85b6f051e4f
trizen <trizen@protonmail.com>
2021-01-30 10:22:15 +0200

    new file:   oeis-research/Amiram Eldar/a(n) is the least number k such that the n-th odd squarefree divides lcm(1..k) over denom(harmonic(k))/prog.sf new file:   oeis-research/Amiram Eldar/a(n) is the least number k such that the n-th odd squarefree divides lcm(1..k) over denom(harmonic(k))/search.pl

---
+++ oeis-research/Amiram Eldar/a(n) is the least number k such that the n-th odd squarefree divides lcm(1..k) over denom(harmonic(k))/search.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# a(n) is the least index k such that the n-th odd squarefree number A056911(n) divides A110566(k).
+# https://oeis.org/A338120
+
+# Search for a(35).
+
+use 5.014;
+use Math::AnyNum qw(lcm inv);
+
+my $v = 85;     # == A056911(35)
+
+my $L = 1;      # LCM
+my $H = 0;      # Harmonic
+
+foreach my $k (1..1e8) {
+
+    say "Testing: $k";
+
+    $L = lcm($L, $k);
+    $H += inv($k);
+
+    if (($L / $H->denominator) % $v == 0) {
+        die "\nFound: $k\n";
+    }
+}
